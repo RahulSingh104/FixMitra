f21d120a1873f9f97d0cd4d861603d283b0a5443
RahulSingh104 <rahulsingh96287@gmail.com>
2026-02-20 21:07:50 +0530

    live  debugging

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,28 +1,65 @@
+// import axios from "axios"
+
+// const API = axios.create({
+//   baseURL: import.meta.env.VITE_API_URL,
+// })
+
+// console.log("API base URL:", import.meta.env.VITE_API_URL);
+
+// API.interceptors.request.use((config) => {
+//   const role = localStorage.getItem("activeRole");
+//   const token = localStorage.getItem(`token_${role}`);
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`
+//   }
+//   return config
+// })
+// API.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem("token")
+//       localStorage.removeItem("user")
+//       window.location.href = "/login"
+//     }
+
+//     return Promise.reject(error)
+//   }
+// )
+
+
+// export default API
+
+
 import axios from "axios"
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
+console.log("API BASE URL:", import.meta.env.VITE_API_URL)
+
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token")
+  const role = localStorage.getItem("activeRole")
+  const token = localStorage.getItem(`token_${role}`)
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
+
   return config
 })
+
 API.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      localStorage.removeItem("token")
-      localStorage.removeItem("user")
+      localStorage.clear()
       window.location.href = "/login"
     }
 
     return Promise.reject(error)
   }
 )
-
 
 export default API
